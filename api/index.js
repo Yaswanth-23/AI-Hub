097d2f776a30cb76f3002357f65aac83752bb5c5
Yaswanth Dev <byyaswanth0023@gmail.com>
2026-03-08 22:33:18 +0530

    Fix serverless function mapping and CORS

--- api/index.js
+++ api/index.js
@@ -21,12 +21,17 @@ export default async function handler(req, res) {
 
     res.setHeader("Access-Control-Allow-Origin", origin);
     res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    if (req.method === "OPTIONS") {
+      return res.status(200).end();
+    }
 
     return server(req, res);
 
   } catch (error) {
     console.error(error);
-
     return res.status(500).json({
       success: false,
       message: "Internal Server Error"
